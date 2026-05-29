479ddf425bbf1f178d48a3b27844e30d7e9ed006
richarddigo <richarddigo@gmail.com>
2026-05-29 20:19:58 +0200

    fix: disable locale detection and remove locale cookie configuration

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,11 +4,7 @@ export const routing = defineRouting({
   locales: ["en", "es", "de", "fr"],
   defaultLocale: "en",
   localePrefix: "as-needed",
-  localeDetection: true,
-  localeCookie: {
-    name: "NEXT_LOCALE",
-    maxAge: 60 * 60 * 24 * 365 * 2, // 2 years
-  },
+  localeDetection: false,
 });
 
 export type Locale = (typeof routing.locales)[number];
